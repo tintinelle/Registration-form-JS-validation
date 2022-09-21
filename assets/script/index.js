5afab4a2e65ae706f968bdaa137e7b94e0ece826
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-09-21 14:43:37 +0500

    }

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,34 +8,46 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const reEnterPassword = document.getElementById('reEnterPassword');
 const birthYear = document.getElementById('birthYear');
-const checkbox = document.getElementById("terms");
+const checkbox = document.getElementById('terms');
 
 let errors = [];
+
+    // Ввожу переменную для проверки валидации, в каждом кейсе с ошибкой isOk будет false
+let isOk = true; 
 
 const checkValidity = (input) => {
     let validity = input.validity;
 
+    isOk = true;
+
     if (validity.valueMissing) {
         errors.push(`Please fill in the ${input.name} field.`);
+
+        isOk = false;
     }
 
     if (validity.typeMismatch) {
         errors.push(`Please enter a valid ${input.name}`);
+        isOk = false;
     }
 
     if (validity.rangeOverflow) {
         const max = input.max;
         errors.push('Your year of birth should be less than ' + max);
+        isOk = false;
     }
 
     if (validity.rangeUnderflow) {
         const min = input.min;
         errors.push('Your year of birth should be more than ' + min);
+        isOk = false;
     }
 
     if (validity.tooShort) {
         errors.push(`Your ${input.name} is too short.`);
+        isOk = false;
     }
+
 };
 
 const checkValidLogin = () => {
@@ -44,6 +56,7 @@ const checkValidLogin = () => {
     console.log(login.value);
     if (!valid) {
         errors.push('Your Login should be from 3 to 16 characters long and can only contain letters, numbers or "-" and "_".');
+        isOk = false;
     }
 };
 
@@ -52,6 +65,7 @@ const checkValidEMail = () => {
     const valid = reg.test(email.value);
     if (!valid) {
         errors.push(`Please enter a valid Email address.`);
+        isOk = false;
     }
 };
 
@@ -60,12 +74,14 @@ const checkStrongPassword = () => {
     const valid = reg.test(password.value);
     if (!valid) {
         errors.push(`Your Password is not strong enough.`);
+        isOk = false;
     }
 };
 
 const checkEqualPassword = () => {
     if (password.value != reEnterPassword.value) {
         errors.push(`Please enter the same passwords in both password fields.`);
+        isOk = false;
     }
 };
 
@@ -74,12 +90,14 @@ const checkAge = () => {
 
     if (age < 13) {
         errors.push(`Sorry, you must be at least 13 years old in order to use this website.`);
+        isOk = false;
     }
 };
 
 const checkCheckbox = () => {
     if (checkbox.checked !== true) {
         errors.push('You must agree to the Steam Subscriber Agreement to continue.');
+        isOk = false;
     }
 };
 
@@ -102,7 +120,13 @@ const checkAll = () => {
 
     errorDiv.innerHTML = errors.join('</br>');
 
-    if (errorDiv.innerHTML === '') {
+    // Проверка валидности на случай, если переменная isOk подкачает
+    // if (errorDiv.innerHTML === '') {
+    //     alert(`Welcome to the website, ${login.value}!`);
+    // }
+
+    // Конечная проверка на валидность по всем пунктам
+    if (isOk) {
         alert(`Welcome to the website, ${login.value}!`);
     }
 };
